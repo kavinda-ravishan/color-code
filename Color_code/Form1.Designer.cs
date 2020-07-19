660d539e278a94867dec0311c50f48596bb84aff
kavinda ravishan <kvnravishan@outlook.com>
2020-07-19 20:27:40 +0530

    Init

---
+++ Color_code/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace Color_code
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(775, 20);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(12, 38);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(775, 539);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 587);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Color Code";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ Color_code/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Color_code
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private int window_width;
+        private int window_height;
+
+        Color ColorMap(double value)
+        {
+            double R;
+            double G;
+            double B;
+
+            int dark = 150;
+
+            if(value >=-1 && value < -0.5)
+            {
+                B = 255;
+                G = ((2 * value) + 2) * 255;
+                R = 0;
+            }
+            else if(value >= -0.5 && value < 0)
+            {
+                B = -2 * value * 255;
+                G = 255;
+                R = 0;
+            }
+            else if (value >= 0 && value < 0.5)
+            {
+                B = 0;
+                G = 255;
+                R = 2 * value * 255;
+            }
+            else if (value >= 0.5 && value <= 1)
+            {
+                B = 0;
+                G = ((-2 * value) + 2) * 255;
+                R = 255;
+            }
+            else
+            {
+                R = 255;
+                G = 255;
+                B = 255;
+            }
+
+            R = (R / 255) * dark;
+            G = (G / 255) * dark;
+            B = (B / 255) * dark;
+
+            return Color.FromArgb((int)R, (int)G, (int)B);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            window_width = pictureBox1.Width;
+            window_height = pictureBox1.Height;
+
+            Bitmap bitmap = new Bitmap(window_width, window_height);
+
+            for (int i = 0; i < window_width; i++)
+            {
+                Color color = ColorMap(((((double)i) / window_width) * 2) - 1);
+
+                for (int j = 0; j < window_height; j++)
+                {
+                    bitmap.SetPixel(i, j, color);
+                }
+            }
+
+            pictureBox1.Image = bitmap;
+
+            pictureBox1.Refresh();
+
+
+            Bitmap img = new Bitmap("ex.jpg");
+
+            Bitmap newImg = new Bitmap(img);
+
+            for(int i = 0; i < img.Width; i++)
+            {
+                for (int j = 0; j < img.Height; j++)
+                {
+                    int R = img.GetPixel(i, j).R;
+                    newImg.SetPixel(i, j, ColorMap((((double)R / 255) * 2) - 1));
+                }
+            }
+
+            pictureBox2.Image = newImg;
+
+            pictureBox2.Refresh();
+        }
+
+        
+    }
+}
